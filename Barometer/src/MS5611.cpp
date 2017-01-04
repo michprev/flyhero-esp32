7e18c92a2551cc4f421dd255c95bbfcdd5025dd0
Michal Prevratil <michprev@gmail.com>
2017-01-05 00:48:45 +0100

    Split data reading to multiple functions so that it is not needed to wait

--- Barometer/src/MS5611.cpp
+++ Barometer/src/MS5611.cpp
@@ -9,6 +9,13 @@ MS5611* MS5611::Instance() {
 	return pInstance;
 }
 
+MS5611::MS5611() {
+	this->D1_Timestamp = 0;
+	this->D2_Timestamp = 0;
+	this->D1 = 0;
+	this->D2 = 0;
+}
+
 HAL_StatusTypeDef MS5611::Reset()
 {
 	uint8_t reset = 0x1E;
@@ -41,32 +48,56 @@ HAL_StatusTypeDef MS5611::Init()
 	return HAL_OK;
 }
 
+bool MS5611::D1_Ready() {
+	if (this->D1_Timestamp != 0 && HAL_GetTick() - this->D1_Timestamp >= 10) {
+		uint8_t conversionData[3];
+		HAL_I2C_Mem_Read(&this->hi2c, this->ADDRESS, 0x00, I2C_MEMADD_SIZE_8BIT, conversionData, 3, HAL_MAX_DELAY);
+		this->D1 = conversionData[0] << 16;
+		this->D1 |= conversionData[1] << 8;
+		this->D1 |= conversionData[2];
+
+		this->D1_Timestamp = 0;
+
+		return true;
+	}
+
+	return false;
+}
+
+bool MS5611::D2_Ready() {
+	if (this->D2_Timestamp != 0 && HAL_GetTick() - this->D2_Timestamp >= 10) {
+		uint8_t conversionData[3];
+		HAL_I2C_Mem_Read(&this->hi2c, this->ADDRESS, 0x00, I2C_MEMADD_SIZE_8BIT, conversionData, 3, HAL_MAX_DELAY);
+		this->D2 = conversionData[0] << 16;
+		this->D2 |= conversionData[1] << 8;
+		this->D2 |= conversionData[2];
+
+		this->D2_Timestamp = 0;
+
+		return true;
+	}
+
+	return false;
+}
+
+HAL_StatusTypeDef MS5611::ConvertD1() {
+	uint8_t convertD1 = 0x48;
+	HAL_StatusTypeDef status = HAL_I2C_Master_Transmit(&this->hi2c, this->ADDRESS, &convertD1, 1, HAL_MAX_DELAY);
+	this->D1_Timestamp = HAL_GetTick();
+
+	return status;
+}
+
+HAL_StatusTypeDef MS5611::ConvertD2() {
+	uint8_t convertD2 = 0x58;
+	HAL_StatusTypeDef status = HAL_I2C_Master_Transmit(&this->hi2c, this->ADDRESS, &convertD2, 1, HAL_MAX_DELAY);
+	this->D2_Timestamp = HAL_GetTick();
+
+	return status;
+}
+
 HAL_StatusTypeDef MS5611::GetData(int32_t * temperature, int32_t * pressure)
 {
-	HAL_StatusTypeDef status;
-	uint32_t D1, D2;
-	uint8_t conversionData[3];
-
-	uint8_t convertD1 = 0x48;
-	status = HAL_I2C_Master_Transmit(&this->hi2c, this->ADDRESS, &convertD1, 1, HAL_MAX_DELAY);
-
-	HAL_Delay(10);
-
-	status = HAL_I2C_Mem_Read(&this->hi2c, this->ADDRESS, 0x00, I2C_MEMADD_SIZE_8BIT, conversionData, 3, HAL_MAX_DELAY);
-	D1 = conversionData[0] << 16;
-	D1 |= conversionData[1] << 8;
-	D1 |= conversionData[2];
-
-	uint8_t convertD2 = 0x58;
-	status = HAL_I2C_Master_Transmit(&this->hi2c, this->ADDRESS, &convertD2, 1, HAL_MAX_DELAY);
-
-	HAL_Delay(10);
-
-	status = HAL_I2C_Mem_Read(&this->hi2c, this->ADDRESS, 0x00, I2C_MEMADD_SIZE_8BIT, conversionData, 3, HAL_MAX_DELAY);
-	D2 = conversionData[0] << 16;
-	D2 |= conversionData[1] << 8;
-	D2 |= conversionData[2];
-
 	int32_t dT = D2 - (uint32_t)C[4] * pow(2, 8);
 	int32_t TEMP = 2000 + dT * C[5] / pow(2, 23);
 	int64_t OFF = (int64_t)C[1] * pow(2, 16) + (int64_t)(C[3] * dT) / pow(2, 7);
@@ -117,7 +148,7 @@ HAL_StatusTypeDef MS5611::I2C_Init()
 
 	this->hi2c.Instance = I2C1;
 	this->hi2c.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-	this->hi2c.Init.ClockSpeed = 100000;
+	this->hi2c.Init.ClockSpeed = 400000;
 	this->hi2c.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
 	this->hi2c.Init.DutyCycle = I2C_DUTYCYCLE_2;
 	this->hi2c.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
