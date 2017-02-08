2f73cf5345425e9b3545244ded7df08ba204c8b9
Michal Prevratil <michprev@gmail.com>
2017-02-08 21:29:10 +0100

    Add sample code for PWM

--- PWM/src/main.cpp
+++ PWM/src/main.cpp
@@ -12,13 +12,24 @@
 #include "stm32f4xx.h"
 #include "stm32f4xx_nucleo.h"
 #include "stm32f4xx_hal.h"
+#include "PWM_Generator.h"
 			
 extern "C" void initialise_monitor_handles(void);
 
 int main(void)
 {
 	HAL_Init();
-	initialise_monitor_handles();
+	//initialise_monitor_handles();
+
+	PWM_Generator *pwm = PWM_Generator::Instance();
+
+	pwm->Init();
+	pwm->Arm(NULL);
+
+	pwm->SetPulse(1050, 1);
+	pwm->SetPulse(1050, 2);
+	pwm->SetPulse(1050, 3);
+	pwm->SetPulse(1050, 4);
 
 	for(;;);
 }
