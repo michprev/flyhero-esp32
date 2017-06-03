7977e4763dbb1f33784dad49edab756462fdd539
michprev <michprev@gmail.com>
2017-06-03 11:02:30 +0200

    Start using new IMU project; don't init I2C in main;

--- The_Eye/src/main.cpp
+++ The_Eye/src/main.cpp
@@ -30,7 +30,6 @@ MS5611 *ms5611 = MS5611::Instance();
 NEO_M8N *neo = NEO_M8N::Instance();
 Logger *logger = Logger::Instance();
 
-void InitI2C(I2C_HandleTypeDef*);
 void Arm_Callback();
 void IPD_Callback(uint8_t link_ID, uint8_t *data, uint16_t length);
 void Calculate();
@@ -58,6 +57,23 @@ MPU6050::Sensor_Data euler_data;
 
 /* End Testing */
 
+MPU6050::Raw_Data gyroData, accelData;
+
+/*uint32_t test;
+uint32_t aa[1000];
+uint32_t ppos = 0;
+
+extern "C" void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart) {
+	if (huart->Instance == UART5) {
+		aa[ppos] = Timer::Get_Tick_Count() - test;
+		ppos++;
+
+		if (ppos == 1000) {
+			printf("a");
+		}
+	}
+}*/
+
 int main(void)
 {
 	HAL_Init();
@@ -84,9 +100,7 @@ int main(void)
 	hiwdg.Init.Reload = 480;
 	// timeout after 2 s
 
-	I2C_HandleTypeDef hI2C_Handle;
-	InitI2C(&hI2C_Handle);
-
+	logger->Init();
 	esp->Init(&IPD_Callback);
 
 	timestamp = HAL_GetTick();
@@ -107,44 +121,10 @@ int main(void)
 	pwm->Arm(&Arm_Callback);
 
 	// reset gyro
-	if (mpu->Init(&hI2C_Handle)) {
+	if (mpu->Init(false)) {
 		LEDs::TurnOn(LEDs::Yellow);
 		while (true);
 	}
-
-	if (mpu->SelfTest())
-		LEDs::TurnOn(LEDs::Green);
-	else
-		LEDs::TurnOn(LEDs::Yellow);
-
-	LEDs::TurnOff(LEDs::Green);
-
-	mpu->CheckNewData();
-	mpu->ReadEuler(&euler_data);
-
-	double x = euler_data.z;
-	double dx, dt;
-	dx = dt = 1;
-	timestamp = HAL_GetTick();
-
-	// wait until calibration of yaw done
-	do {
-		mpu->CheckNewData();
-
-		if (HAL_GetTick() - timestamp >= 500) {
-			dt = HAL_GetTick() - timestamp;
-			mpu->ReadEuler(&euler_data);
-
-			dx = euler_data.z - x;
-
-			x = euler_data.z;
-			timestamp = HAL_GetTick();
-		}
-
-		esp->Process_Data();
-	} while (fabs(dx / dt) > 0.0001);
-
-	reference_yaw = euler_data.z;
 
 	pwm->SetPulse(1100, 1);
 	pwm->SetPulse(1100, 2);
@@ -164,7 +144,6 @@ int main(void)
 
 			timestamp = HAL_GetTick();
 		}
-		mpu->CheckNewData();
 		esp->Process_Data();
 	}
 
@@ -185,113 +164,104 @@ int main(void)
 	LEDs::TurnOn(LEDs::Green);
 
 #ifndef LOG
-	HAL_IWDG_Init(&hiwdg);
+	//HAL_IWDG_Init(&hiwdg);
 #endif
 
 	throttle = 0;
+	mpu->ready = true;
 
 	while (true) {
-		status = mpu->CheckNewData();
+		if (mpu->Data_Ready()) {
+			mpu->Start_Read_Raw();
+			//mpu->Read_Raw(&gyroData, &accelData);
+		}
 
-		if (status == 1) {
-			if (data_received)
-				HAL_IWDG_Refresh(&hiwdg);
+		if (mpu->Data_Read()) {
+			//if (data_received)
+			//	HAL_IWDG_Refresh(&hiwdg);
 			data_received = false;
 
+			mpu->Complete_Read_Raw(&gyroData, &accelData);
 
-			//mpu->ReadAccel(&accel_data);
-			//mpu->ReadGyro(&gyro_data);
-			mpu->ReadEuler(&euler_data);
+			/*uint8_t tmp[15];
+			tmp[0] = accelData.x >> 8;
+			tmp[1] = accelData.x & 0xFF;
+			tmp[2] = accelData.y >> 8;
+			tmp[3] = accelData.y & 0xFF;
+			tmp[4] = accelData.z >> 8;
+			tmp[5] = accelData.z & 0xFF;
+			tmp[6] = gyroData.x >> 8;
+			tmp[7] = gyroData.x & 0xFF;
+			tmp[8] = gyroData.y >> 8;
+			tmp[9] = gyroData.y & 0xFF;
+			tmp[10] = gyroData.z >> 8;
+			tmp[11] = gyroData.z & 0xFF;
+			tmp[12] = throttle >> 8;
+			tmp[13] = throttle & 0xFF;
+			tmp[14] = 0;
 
-			//Calculate();
+			for (uint8_t i = 0; i <= 13; i++)
+				tmp[14] ^= tmp[i];
 
-			data[0] = euler_data.x;
-			data[1] = euler_data.y;
-			data[2] = euler_data.z;
+			test = Timer::Get_Tick_Count();
+			logger->Print(tmp, 15);*/
 
 
-			/*uint8_t logData[16];
-			int32_t tdata[3];
-			tdata[0] = data[0] * 65536.f;
-			tdata[1] = data[1] * 65536.f;
-			tdata[2] = data[2] * 65536.f;
+			data[0] = 0;
+			data[1] = 0;
+			data[2] = 0;
 
-			logData[0] = (tdata[0] >> 24) & 0xFF;
-			logData[1] = (tdata[0] >> 16) & 0xFF;
-			logData[2] = (tdata[0] >> 8) & 0xFF;
-			logData[3] = tdata[0] & 0xFF;
-			logData[4] = (tdata[1] >> 24) & 0xFF;
-			logData[5] = (tdata[1] >> 16) & 0xFF;
-			logData[6] = (tdata[1] >> 8) & 0xFF;
-			logData[7] = tdata[1] & 0xFF;
-			logData[8] = (tdata[2] >> 24) & 0xFF;
-			logData[9] = (tdata[2] >> 16) & 0xFF;
-			logData[10] = (tdata[2] >> 8) & 0xFF;
-			logData[11] = tdata[2] & 0xFF;
-			logData[12] = (throttle >> 24) & 0xFF;
-			logData[13] = (throttle >> 16) & 0xFF;
-			logData[14] = (throttle >> 8) & 0xFF;
-			logData[15] = throttle & 0xFF;
+			if (throttle >= 1050) {
+				pitchCorrection = PID_Pitch.get_pid(data[1], 1);
+				rollCorrection = PID_Roll.get_pid(data[0], 1);
+				yawCorrection = PID_Yaw.get_pid(data[2] - reference_yaw, 1);
 
-			esp->Get_Connection('4')->Connection_Send_Begin(logData, 16);
-			while (esp->Get_Connection('4')->Get_State() != CONNECTION_READY && esp->Get_Connection('4')->Get_State() != CONNECTION_CLOSED) {
-					esp->Get_Connection('4')->Connection_Send_Continue();
-			}*/
-		}
+				// not sure about yaw signs
+				if (!inverse_yaw) {
+					FL = throttle + rollCorrection + pitchCorrection + yawCorrection; // PB2
+					BL = throttle + rollCorrection - pitchCorrection - yawCorrection; // PA15
+					FR = throttle - rollCorrection + pitchCorrection - yawCorrection; // PB10
+					BR = throttle - rollCorrection - pitchCorrection + yawCorrection; // PA1
+				}
+				else {
+					FL = throttle + rollCorrection + pitchCorrection - yawCorrection; // PB2
+					BL = throttle + rollCorrection - pitchCorrection + yawCorrection; // PA15
+					FR = throttle - rollCorrection + pitchCorrection + yawCorrection; // PB10
+					BR = throttle - rollCorrection - pitchCorrection - yawCorrection; // PA1
+				}
 
-		if (status == 1 && throttle >= 1050) {
-			pitchCorrection = PID_Pitch.get_pid(data[1], 1);
-			rollCorrection = PID_Roll.get_pid(data[0], 1);
-			yawCorrection = PID_Yaw.get_pid(data[2] - reference_yaw, 1);
+				if (FL > 2000)
+					FL = 2000;
+				else if (FL < 1050)
+					FL = 940;
 
-			// not sure about yaw signs
-			if (!inverse_yaw) {
-				FL = throttle + rollCorrection + pitchCorrection + yawCorrection; // PB2
-				BL = throttle + rollCorrection - pitchCorrection - yawCorrection; // PA15
-				FR = throttle - rollCorrection + pitchCorrection - yawCorrection; // PB10
-				BR = throttle - rollCorrection - pitchCorrection + yawCorrection; // PA1
+				if (BL > 2000)
+					BL = 2000;
+				else if (BL < 1050)
+					BL = 940;
+
+				if (FR > 2000)
+					FR = 2000;
+				else if (FR < 1050)
+					FR = 940;
+
+				if (BR > 2000)
+					BR = 2000;
+				else if (BR < 1050)
+					BR = 940;
+
+				pwm->SetPulse(FL, 3);
+				pwm->SetPulse(BL, 2);
+				pwm->SetPulse(FR, 4);
+				pwm->SetPulse(BR, 1);
 			}
 			else {
-				FL = throttle + rollCorrection + pitchCorrection - yawCorrection; // PB2
-				BL = throttle + rollCorrection - pitchCorrection + yawCorrection; // PA15
-				FR = throttle - rollCorrection + pitchCorrection + yawCorrection; // PB10
-				BR = throttle - rollCorrection - pitchCorrection - yawCorrection; // PA1
+				pwm->SetPulse(940, 4);
+				pwm->SetPulse(940, 1);
+				pwm->SetPulse(940, 3);
+				pwm->SetPulse(940, 2);
+				reference_yaw = euler_data.z;
 			}
-
-			if (FL > 2000)
-				FL = 2000;
-			else if (FL < 1050)
-				FL = 940;
-
-			if (BL > 2000)
-				BL = 2000;
-			else if (BL < 1050)
-				BL = 940;
-
-			if (FR > 2000)
-				FR = 2000;
-			else if (FR < 1050)
-				FR = 940;
-
-			if (BR > 2000)
-				BR = 2000;
-			else if (BR < 1050)
-				BR = 940;
-
-			pwm->SetPulse(FL, 3);
-			pwm->SetPulse(BL, 2);
-			pwm->SetPulse(FR, 4);
-			pwm->SetPulse(BR, 1);
-		}
-		else if (status == 2)
-			LEDs::TurnOn(LEDs::Orange);
-
-		if (throttle < 1050) {
-			pwm->SetPulse(940, 4);
-			pwm->SetPulse(940, 1);
-			pwm->SetPulse(940, 3);
-			pwm->SetPulse(940, 2);
-			reference_yaw = euler_data.z;
 		}
 
 		esp->Process_Data();
@@ -371,40 +341,6 @@ void IPD_Callback(uint8_t link_ID, uint8_t *data, uint16_t length) {
 void Arm_Callback() {
 	esp->Process_Data();
 	//HAL_IWDG_Refresh(&hiwdg);
-}
-
-void InitI2C(I2C_HandleTypeDef *I2C_Handle) {
-	if (__GPIOB_IS_CLK_DISABLED())
-		__GPIOB_CLK_ENABLE();
-
-	if (__I2C1_IS_CLK_DISABLED())
-		__I2C1_CLK_ENABLE();
-
-	GPIO_InitTypeDef GPIO_InitStruct;
-	GPIO_InitStruct.Pin = GPIO_PIN_8 | GPIO_PIN_9;
-	GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
-	GPIO_InitStruct.Pull = GPIO_PULLUP;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-	GPIO_InitStruct.Alternate = GPIO_AF4_I2C1;
-	HAL_GPIO_DeInit(GPIOB, GPIO_PIN_8 | GPIO_PIN_9);
-	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-	I2C_Handle->Instance = I2C1;
-	I2C_Handle->Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-	I2C_Handle->Init.ClockSpeed = 400000;
-	I2C_Handle->Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-	I2C_Handle->Init.DutyCycle = I2C_DUTYCYCLE_2;
-	I2C_Handle->Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-	I2C_Handle->Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-	I2C_Handle->Init.OwnAddress1 = 0;
-	I2C_Handle->Init.OwnAddress2 = 0;
-
-	__HAL_I2C_RESET_HANDLE_STATE(I2C_Handle);
-
-	if (HAL_I2C_DeInit(I2C_Handle) != HAL_OK)
-		LEDs::TurnOn(LEDs::Orange);
-	if (HAL_I2C_Init(I2C_Handle) != HAL_OK)
-		LEDs::TurnOn(LEDs::Orange);
 }
 
 /*void Calculate() {
