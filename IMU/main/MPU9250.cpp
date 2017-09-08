e948405a8739ed0521ddfb10ba172c38c1287854
michprev <michprev@gmail.com>
2017-09-08 18:54:29 +0200

    Implement MPU9250 device class with basic sensor configuration

---
+++ IMU/main/MPU9250.cpp
@@ -0,0 +1,302 @@
+/*
+ * MPU9250.cpp
+ *
+ *  Created on: 7. 9. 2017
+ *      Author: michp
+ */
+
+#include "MPU9250.h"
+
+namespace flyhero {
+
+MPU9250& MPU9250::Instance() {
+	static MPU9250 instance;
+
+	return instance;
+}
+
+MPU9250::MPU9250() {
+	this->spi = NULL;
+	this->rx_buffer = NULL;
+}
+
+esp_err_t MPU9250::spi_init() {
+    esp_err_t ret;
+
+
+    spi_bus_config_t buscfg;
+    buscfg.miso_io_num = GPIO_NUM_25;
+    buscfg.mosi_io_num = GPIO_NUM_23;
+    buscfg.sclk_io_num = GPIO_NUM_19;
+    buscfg.quadwp_io_num = -1;
+    buscfg.quadhd_io_num = -1;
+    buscfg.max_transfer_sz = 0;
+
+    spi_device_interface_config_t devcfg;
+    devcfg.command_bits = 0;
+    devcfg.address_bits = 8;
+    devcfg.dummy_bits = 0;
+    devcfg.mode = 0;
+    devcfg.duty_cycle_pos = 128;
+    devcfg.cs_ena_pretrans = 0;
+    devcfg.cs_ena_posttrans = 0;
+    devcfg.clock_speed_hz = 1000000;
+    devcfg.spics_io_num = GPIO_NUM_22;
+    devcfg.flags = 0;
+    devcfg.queue_size = 7;
+    devcfg.pre_cb = 0;
+    devcfg.post_cb = 0;
+
+    //Initialize the SPI bus
+    if ( (ret = spi_bus_initialize(HSPI_HOST, &buscfg, 1)) )
+    	return ret;
+
+    if ( (ret = spi_bus_add_device(HSPI_HOST, &devcfg, &this->spi)) )
+    	return ret;
+
+    return ESP_OK;
+}
+
+esp_err_t MPU9250::spi_reg_read(uint8_t reg, uint8_t& data) {
+	esp_err_t ret;
+
+	spi_transaction_t trans;
+	trans.flags = SPI_TRANS_USE_RXDATA | SPI_TRANS_USE_TXDATA;
+	trans.cmd = 0;
+	trans.addr = reg | 0x80;
+	trans.length = 8;
+	trans.rxlength = 8;
+	trans.user = 0;
+	trans.tx_data[0] = 0x00;
+
+	if ( (ret = spi_device_transmit(this->spi, &trans)) )
+		return ret;
+
+	data = trans.rx_data[0];
+
+	return ESP_OK;
+}
+
+DRAM_ATTR static const uint8_t tx_dummy[40] = { 0x00 };
+
+esp_err_t MPU9250::spi_regs_read(uint8_t first_reg, uint8_t count) {
+	//return ESP_ERR_NOT_SUPPORTED;
+
+	count += 4 - (count % 4);
+
+	esp_err_t ret;
+
+	spi_transaction_t trans;
+	trans.flags = 0;
+	trans.cmd = 0;
+	trans.addr = first_reg | 0x80;
+	trans.length = count * 8;
+	trans.rxlength = count * 8;
+	trans.user = 0;
+	trans.tx_buffer = tx_dummy;
+	trans.rx_buffer = this->rx_buffer;
+
+	if ( (ret = spi_device_transmit(this->spi, &trans)) )
+		return ret;
+
+	return ESP_OK;
+}
+
+esp_err_t MPU9250::spi_reg_write(uint8_t reg, uint8_t data) {
+	esp_err_t ret;
+
+	spi_transaction_t trans;
+	trans.flags = SPI_TRANS_USE_TXDATA;
+	trans.cmd = 0;
+	trans.addr = reg & 0x7F;
+	trans.length = 8;
+	trans.rxlength = 0;
+	trans.user = 0;
+	trans.tx_data[0] = data;
+
+	if ( (ret = spi_device_transmit(this->spi, &trans)) )
+		return ret;
+
+	return ESP_OK;
+}
+
+void MPU9250::set_gyro_fsr(gyro_fsr fsr) {
+	if (fsr == GYRO_FSR_NOT_SET)
+		return;
+
+	if (fsr == this->g_fsr)
+		return;
+
+
+	// gyro FSR config shares the same register with DLPF config so we just update reg value
+	uint8_t gyro_config;
+
+	this->spi_reg_read(this->REGISTERS.GYRO_CONFIG, gyro_config);
+	this->spi_reg_write(this->REGISTERS.GYRO_CONFIG, gyro_config | fsr);
+
+	this->g_fsr = fsr;
+
+	switch (this->g_fsr) {
+	case GYRO_FSR_250:
+		this->g_mult = 250;
+		break;
+	case GYRO_FSR_500:
+		this->g_mult = 500;
+		break;
+	case GYRO_FSR_1000:
+		this->g_mult = 1000;
+		break;
+	case GYRO_FSR_2000:
+		this->g_mult = 2000;
+		break;
+	case GYRO_FSR_NOT_SET:
+		return;
+	}
+
+	this->g_mult /= std::pow(2, this->ADC_BITS - 1);
+}
+
+void MPU9250::set_accel_fsr(accel_fsr fsr) {
+	if (fsr == ACCEL_FSR_NOT_SET)
+		return;
+
+	if (fsr == this->a_fsr)
+		return;
+
+	this->spi_reg_write(this->REGISTERS.ACCEL_CONFIG, fsr);
+
+	this->a_fsr = fsr;
+
+	switch (this->a_fsr) {
+	case ACCEL_FSR_2:
+		this->a_mult = 2;
+		break;
+	case ACCEL_FSR_4:
+		this->a_mult = 4;
+		break;
+	case ACCEL_FSR_8:
+		this->a_mult = 8;
+		break;
+	case ACCEL_FSR_16:
+		this->a_mult = 16;
+		break;
+	case ACCEL_FSR_NOT_SET:
+		return;
+	}
+
+	this->a_mult /= std::pow(2, this->ADC_BITS - 1);
+}
+
+void MPU9250::set_gyro_lpf(gyro_lpf lpf) {
+	if (lpf == GYRO_LPF_NOT_SET)
+		return;
+
+	if (this->g_lpf == lpf)
+		return;
+
+	uint8_t Fchoice_b;
+
+	if (lpf == GYRO_LPF_8800HZ)
+		Fchoice_b = 0x01;
+	else if (lpf == GYRO_LPF_3600Hz)
+		Fchoice_b = 0x02;
+	else
+		Fchoice_b = 0x00;
+
+	// gyro DLPF config shares the same register with FSR config so we just update reg value
+	uint8_t gyro_config;
+
+	this->spi_reg_read(this->REGISTERS.GYRO_CONFIG, gyro_config);
+	this->spi_reg_write(this->REGISTERS.GYRO_CONFIG, gyro_config | Fchoice_b);
+
+	this->spi_reg_write(this->REGISTERS.CONFIG, lpf);
+
+	this->g_lpf = lpf;
+}
+
+void MPU9250::set_accel_lpf(accel_lpf lpf) {
+	if (lpf == ACCEL_LPF_NOT_SET)
+		return;
+
+	if (lpf == this->a_lpf)
+		return;
+
+	uint8_t accel_config2 = lpf;
+
+	if (lpf == ACCEL_LPF_1046HZ)
+		accel_config2 |= 0x08;
+
+	this->spi_reg_write(this->REGISTERS.ACCEL_CONFIG2, accel_config2);
+
+	this->a_lpf = lpf;
+}
+
+void MPU9250::Init() {
+	esp_err_t ret;
+
+	this->rx_buffer = (uint8_t*)heap_caps_malloc(40, MALLOC_CAP_32BIT | MALLOC_CAP_DMA);
+
+	if (this->rx_buffer == NULL)
+		std::cout << "alloc error" << std::endl;
+
+	if ( (ret = this->spi_init()) )
+		return;
+
+	// reset the device
+	this->spi_reg_write(this->REGISTERS.PWR_MGMT_1, 0x80);
+
+	// wait until reset done
+	uint8_t tmp;
+	do {
+		this->spi_reg_read(this->REGISTERS.PWR_MGMT_1, tmp);
+	} while (tmp & 0x80);
+
+	vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+	/* should not be needed
+	this->spi_reg_write(this->REGISTERS.SIGNAL_PATH_RESET, 0x07);
+	*/
+
+	// disable FIFO, I2C master; SPI mode only; reset all signal paths
+	this->spi_reg_write(this->REGISTERS.USER_CTRL, 0x2F);
+
+	// enable all sensors
+	this->spi_reg_write(this->REGISTERS.PWR_MGMT_2, 0x00);
+
+	// check device responding
+	uint8_t who_am_i;
+
+	this->spi_reg_read(this->REGISTERS.WHO_AM_I, who_am_i);
+
+	if (who_am_i != 0x71)
+		return;
+
+
+
+
+
+
+
+
+
+
+	std::cout << "Init complete" << std::endl;
+
+
+	/*timeval start, end;
+
+	gettimeofday(&start, NULL);
+
+	//this->spi_regs_read(this->REGISTERS.ACCEL_XOUT_H, 14);
+
+
+
+	gettimeofday(&end, NULL);
+
+	for (int i = 0; i < 14; i++)
+		std::cout << "data[" << i << "]: " << (int)(this->rx_buffer[i]) << std::endl;
+
+	std::cout << "in " << end.tv_sec - start.tv_sec << " s, " << end.tv_usec - start.tv_usec << " us" << std::endl;*/
+}
+
+} /* namespace flyhero */
---
+++ IMU/main/MPU9250.h
@@ -0,0 +1,118 @@
+/*
+ * MPU9250.h
+ *
+ *  Created on: 7. 9. 2017
+ *      Author: michp
+ */
+
+#pragma once
+
+#include <driver/spi_master.h>
+#include <driver/gpio.h>
+#include <esp_err.h>
+#include <freertos/task.h>
+#include <cmath>
+
+extern "C" {
+
+#include <esp_heap_caps.h>
+
+}
+
+// debug
+#include <iostream>
+#include <sys/time.h>
+#include <unistd.h>
+
+namespace flyhero {
+
+class MPU9250 {
+private:
+	/* Singleton begin */
+	MPU9250();
+	MPU9250(MPU9250 const&);
+	MPU9250& operator=(MPU9250 const&);
+	/*Singleton end */
+
+	enum gyro_fsr {
+		GYRO_FSR_250 		= 0x00,
+		GYRO_FSR_500 		= 0x08,
+		GYRO_FSR_1000 		= 0x10,
+		GYRO_FSR_2000 		= 0x18,
+		GYRO_FSR_NOT_SET 	= 0xFF
+	};
+
+	enum accel_fsr {
+		ACCEL_FSR_2 		= 0x00,
+		ACCEL_FSR_4 		= 0x08,
+		ACCEL_FSR_8 		= 0x10,
+		ACCEL_FSR_16 		= 0x18,
+		ACCEL_FSR_NOT_SET 	= 0xFF
+	};
+
+	enum gyro_lpf {
+		GYRO_LPF_8800HZ 	= 0x00,
+		GYRO_LPF_3600Hz 	= 0x00,
+		GYRO_LPF_250HZ 		= 0x00,
+		GYRO_LPF_184HZ 		= 0x01,
+		GYRO_LPF_92HZ 		= 0x02,
+		GYRO_LPF_41HZ 		= 0x03,
+		GYRO_LPF_20HZ 		= 0x04,
+		GYRO_LPF_10HZ 		= 0x05,
+		GYRO_LPF_5HZ 		= 0x06,
+		GYRO_LPF_NOT_SET 	= 0xFF
+	};
+
+	enum accel_lpf {
+		ACCEL_LPF_1046HZ 	= 0x00,
+		ACCEL_LPF_218HZ 	= 0x00,
+		ACCEL_LPF_99HZ 		= 0x02,
+		ACCEL_LPF_45HZ 		= 0x03,
+		ACCEL_LPF_21HZ 		= 0x04,
+		ACCEL_LPF_10HZ 		= 0x05,
+		ACCEL_LPF_5HZ 		= 0x06,
+		ACCEL_LPF_420HZ 	= 0x07,
+		ACCEL_LPF_NOT_SET 	= 0xFF
+	};
+
+	const struct {
+		uint8_t CONFIG 				= 26;
+		uint8_t GYRO_CONFIG 		= 27;
+		uint8_t ACCEL_CONFIG 		= 28;
+		uint8_t ACCEL_CONFIG2 		= 29;
+		uint8_t ACCEL_XOUT_H 		= 59;
+		uint8_t SIGNAL_PATH_RESET 	= 104;
+		uint8_t USER_CTRL 			= 106;
+		uint8_t PWR_MGMT_1 			= 107;
+		uint8_t PWR_MGMT_2 			= 108;
+		uint8_t WHO_AM_I 			= 117;
+	} REGISTERS;
+
+	const uint8_t ADC_BITS = 16;
+
+
+	gyro_fsr g_fsr;
+	accel_fsr a_fsr;
+	gyro_lpf g_lpf;
+	accel_lpf a_lpf;
+	float g_mult, a_mult;
+	spi_device_handle_t spi;
+	uint8_t *rx_buffer;
+
+	esp_err_t spi_init();
+	esp_err_t spi_reg_read(uint8_t reg, uint8_t& data);
+	esp_err_t spi_regs_read(uint8_t first_reg, uint8_t count);
+	esp_err_t spi_reg_write(uint8_t reg, uint8_t data);
+
+	void set_gyro_fsr(gyro_fsr fsr);
+	void set_accel_fsr(accel_fsr fsr);
+	void set_gyro_lpf(gyro_lpf lpf);
+	void set_accel_lpf(accel_lpf lpf);
+
+public:
+	static MPU9250& Instance();
+
+	void Init();
+};
+
+} /* namespace flyhero */
