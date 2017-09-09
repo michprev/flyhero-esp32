2062d4dcf9d40c08d220d9febf59caec19f62c6f
michprev <michprev@gmail.com>
2017-09-09 20:49:48 +0200

    (main): Test MPU9250 interrupt

--- IMU/main/main.cpp
+++ IMU/main/main.cpp
@@ -1,10 +1,46 @@
 #include "MPU9250.h"
 
+// debug
+#include <iostream>
+#include <sys/time.h>
+
 using namespace flyhero;
 
+void imu_task(void *args) {
+	MPU9250& mpu = MPU9250::Instance();
+	mpu.Init();
+
+	timeval time;
+	uint32_t tmp[10];
+	uint16_t pos = 0;
+
+	bool run = true;
+
+	while (run) {
+		if (mpu.Data_Ready()) {
+			gettimeofday(&time, NULL);
+
+			tmp[pos] = time.tv_usec - tmp[pos];
+
+			pos++;
+
+			if (pos == 10)
+				break;
+
+			tmp[pos] = time.tv_usec;
+		}
+	}
+
+	for (int i = 0; i < 10; i++)
+		std::cout << tmp[i] << std::endl;
+
+	vTaskDelete(NULL);
+}
 
 extern "C" void app_main(void) {
-	MPU9250::Instance().Init();
+
+	xTaskCreatePinnedToCore(imu_task, "IMU task", 2048, NULL, 1, NULL, 1);
+
 }
 
 
