a851733c31b0f8c7f19ef8b1825dac3e898cc89c
michprev <michprev@gmail.com>
2017-09-17 10:13:09 +0200

    (LEDs): implement LEDs class in The Eye

---
+++ The_Eye/main/LEDs.h
@@ -0,0 +1,32 @@
+/*
+ * LEDs.h
+ *
+ *  Created on: 17. 9. 2017
+ *      Author: michp
+ */
+
+#pragma once
+
+#include <driver/gpio.h>
+#include <esp_err.h>
+
+namespace flyhero {
+
+class LEDs {
+private:
+	LEDs() {};
+
+public:
+	enum Color { ONBOARD = 2 << 0 };
+
+	static void Init();
+	static void Turn_On(Color color);
+	static void Turn_Off(Color color);
+
+};
+
+inline LEDs::Color operator|(LEDs::Color a, LEDs::Color b) {
+		return static_cast<LEDs::Color>(static_cast<int>(a) | static_cast<int>(b));
+}
+
+} /* namespace flyhero */
