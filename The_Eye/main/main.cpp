0be3299f81b67b452d246da4d1ec375ea7be094a
michprev <michprev@gmail.com>
2017-10-20 00:39:32 +0200

    (The Eye): implement logging multiple sets of euler data

--- The_Eye/main/main.cpp
+++ The_Eye/main/main.cpp
@@ -21,6 +21,8 @@ void imu_task(void *args);
 
 void wifi_parser(uint8_t *buffer, uint8_t received_length);
 
+const uint8_t FUSION_ALGORITHMS_USED = 2;
+
 extern "C" void app_main(void) {
     // Initialize NVS
     esp_err_t nvs_status = nvs_flash_init();
@@ -34,7 +36,7 @@ extern "C" void app_main(void) {
 
 	LEDs::Init();
 
-	euler_queue = xQueueCreate(20, sizeof(IMU::Euler_Angles));
+	euler_queue = xQueueCreate(20, sizeof(IMU::Euler_Angles[FUSION_ALGORITHMS_USED]));
 
 	xTaskCreatePinnedToCore(wifi_task, "WiFi task", 4096, NULL, 2, NULL, 0);
 	xTaskCreatePinnedToCore(imu_task, "IMU task", 4096, NULL, 2, NULL, 1);
@@ -44,34 +46,38 @@ extern "C" void app_main(void) {
 
 void imu_task(void *args) {
 	IMU::Sensor_Data accel, gyro;
-	IMU::Euler_Angles euler;
+	IMU::Euler_Angles mahony_euler, complementary_euler;
 
 	//MPU9250& mpu = MPU9250::Instance();
 	MPU6050& mpu = MPU6050::Instance();
-	//Mahony_Filter mahony(2, 0.1f, 1000);
+	Mahony_Filter mahony(2, 0.1f, 1000);
 	Complementary_Filter complementary(0.995f, 1000);
 
 	motors_controller.Init();
 	mpu.Init();
 
+    IMU::Euler_Angles queue_data[FUSION_ALGORITHMS_USED];
 	uint8_t i = 0;
 
 	while (true) {
 		if (mpu.Data_Ready()) {
 			mpu.Read_Data(accel, gyro);
 
-			//mahony.Compute(accel, gyro, euler);
-			complementary.Compute(accel, gyro, euler);
+			mahony.Compute(accel, gyro, mahony_euler);
+			complementary.Compute(accel, gyro, complementary_euler);
 
 			i++;
 
 			if (i == 100) {
-				xQueueSend(euler_queue, &euler, 0);
+                queue_data[0] = mahony_euler;
+                queue_data[1] = complementary_euler;
+
+				xQueueSend(euler_queue, &queue_data, 0);
 				i = 0;
 			}
 
 
-			motors_controller.Update_Motors(euler);
+			motors_controller.Update_Motors(mahony_euler);
 		}
 	}
 }
@@ -83,7 +89,7 @@ void wifi_task(void *args) {
 	uint8_t buffer[BUFFER_SIZE];
 	uint8_t received_length;
 
-	IMU::Euler_Angles euler;
+	IMU::Euler_Angles euler[FUSION_ALGORITHMS_USED];
 
 	wifi.Init();
 
@@ -106,33 +112,39 @@ void wifi_task(void *args) {
 
 		if (xQueueReceive(euler_queue, &euler, 0) == pdTRUE) {
 			uint8_t *roll, *pitch, *yaw;
-			roll = (uint8_t*)&euler.roll;
-			pitch = (uint8_t*)&euler.pitch;
-			yaw = (uint8_t*)&euler.yaw;
 
-			uint8_t data[16];
+            // 1 (data size) + 1 (datagram type) + 12 * #_fusion_algorithms_used + 2 (crc)
+            const uint8_t DATA_SIZE = 4 + 12 * FUSION_ALGORITHMS_USED;
+            uint8_t data[DATA_SIZE];
 
-            data[0] = 16;
+            data[0] = DATA_SIZE;
             data[1] = 0x00;
-			data[2] = roll[3];
-			data[3] = roll[2];
-			data[4] = roll[1];
-			data[5] = roll[0];
-			data[6] = pitch[3];
-			data[7] = pitch[2];
-			data[8] = pitch[1];
-			data[9] = pitch[0];
-			data[10] = yaw[3];
-			data[11] = yaw[2];
-			data[12] = yaw[1];
-			data[13] = yaw[0];
 
-			uint16_t crc = CRC::CRC16(data, 14);
+            for (uint8_t i = 0; i < FUSION_ALGORITHMS_USED; i++) {
+                roll = (uint8_t *) &(euler[i].roll);
+                pitch = (uint8_t *) &(euler[i].pitch);
+                yaw = (uint8_t *) &(euler[i].yaw);
 
-            data[14] = crc & 0xFF;
-			data[15] = crc >> 8;
+                data[2 + i * 12] = roll[3];
+                data[3 + i * 12] = roll[2];
+                data[4 + i * 12] = roll[1];
+                data[5 + i * 12] = roll[0];
+                data[6 + i * 12] = pitch[3];
+                data[7 + i * 12] = pitch[2];
+                data[8 + i * 12] = pitch[1];
+                data[9 + i * 12] = pitch[0];
+                data[10 + i * 12] = yaw[3];
+                data[11 + i * 12] = yaw[2];
+                data[12 + i * 12] = yaw[1];
+                data[13 + i * 12] = yaw[0];
+            }
 
-			wifi.Send(data, 16);
+			uint16_t crc = CRC::CRC16(data, DATA_SIZE - 2);
+
+            data[DATA_SIZE - 2] = crc & 0xFF;
+			data[DATA_SIZE - 1] = crc >> 8;
+
+			wifi.Send(data, DATA_SIZE);
 		}
 	}
 }
