6aa31ddbbf1eb9086acd2dc8893f7e371dd05351
michprev <michprev@gmail.com>
2017-11-01 12:46:41 +0100

    (WiFi): fix NVS flash not being initialized

--- WiFi/main/main.cpp
+++ WiFi/main/main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
+#include <nvs_flash.h>
+
 #include "WiFi_Controller.h"
 
 using namespace flyhero;
 
 extern "C" void app_main(void)
 {
+    // Initialize NVS
+    esp_err_t nvs_status = nvs_flash_init();
+
+    if (nvs_status == ESP_ERR_NVS_NO_FREE_PAGES) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        nvs_status = nvs_flash_init();
+    }
+
+    ESP_ERROR_CHECK(nvs_status);
+
 	WiFi_Controller& wifi = WiFi_Controller::Instance();
 	LEDs::Init();
 
