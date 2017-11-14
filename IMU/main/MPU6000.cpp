michprev/flyhero-esp32

commit 41ede7a07683c62b740bcd5c37994b2f75dc628b

(IMU): remove unnecessary delays in sensors

IMU/main/MPU6000.cpp
@@ -280,15 +280,13 @@ void MPU6000::Init()
         this->spi_reg_read(this->REGISTERS.PWR_MGMT_1, tmp);
     } while (tmp & 0x80);
 
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+
     // reset analog devices - should not be needed
     ESP_ERROR_CHECK(this->spi_reg_write(this->REGISTERS.SIGNAL_PATH_RESET, 0x07));
 
-    vTaskDelay(100 / portTICK_RATE_MS);
-
     // wake up, set clock source PLL with Z gyro axis
     ESP_ERROR_CHECK(this->spi_reg_write(this->REGISTERS.PWR_MGMT_1, 0x03));
-
-    vTaskDelay(50 / portTICK_RATE_MS);
 
     // do not disable any sensor
     ESP_ERROR_CHECK(this->spi_reg_write(this->REGISTERS.PWR_MGMT_2, 0x00));
@@ -354,8 +352,6 @@ void MPU6000::Calibrate()
 {
     Raw_Data accel, gyro;
 
-    vTaskDelay(1000 / portTICK_RATE_MS);
-
     for (uint16_t i = 0; i < 500; i++)
     {
         this->Read_Raw(accel, gyro);
@@ -366,8 +362,6 @@ void MPU6000::Calibrate()
         this->gyro_offsets[0] += gyro.x;
         this->gyro_offsets[1] += gyro.y;
         this->gyro_offsets[2] += gyro.z;
-
-        vTaskDelay(1 / portTICK_RATE_MS);
     }
 
     this->accel_offsets[0] /= -500;

IMU/main/MPU6050.cpp
@@ -356,15 +356,13 @@ void MPU6050::Init()
         this->i2c_read(this->REGISTERS.PWR_MGMT_1, &tmp);
     } while (tmp & 0x80);
 
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+
     // reset analog devices - should not be needed
     ESP_ERROR_CHECK(this->i2c_write(this->REGISTERS.SIGNAL_PATH_RESET, 0x07));
 
-    vTaskDelay(100 / portTICK_RATE_MS);
-
     // wake up, set clock source PLL with Z gyro axis
     ESP_ERROR_CHECK(this->i2c_write(this->REGISTERS.PWR_MGMT_1, 0x03));
-
-    vTaskDelay(50 / portTICK_RATE_MS);
 
     // do not disable any sensor
     ESP_ERROR_CHECK(this->i2c_write(this->REGISTERS.PWR_MGMT_2, 0x00));
@@ -490,7 +488,6 @@ void MPU6050::Calibrate()
     this->set_gyro_fsr(prev_g_fsr);
     this->set_accel_fsr(prev_a_fsr);
 
-    vTaskDelay(5000 / portTICK_RATE_MS);
 
     // lets measure offsets again to be applied on STM
     this->accel_offsets[0] = this->accel_offsets[1] = this->accel_offsets[2] = 0;

IMU/main/MPU9250.cpp
@@ -327,7 +327,7 @@ void MPU9250::Init()
         this->spi_reg_read(this->REGISTERS.PWR_MGMT_1, tmp);
     } while (tmp & 0x80);
 
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    vTaskDelay(100 / portTICK_PERIOD_MS);
 
     /* should not be needed
     this->spi_reg_write(this->REGISTERS.SIGNAL_PATH_RESET, 0x07);
@@ -393,8 +393,6 @@ void MPU9250::Init()
 void MPU9250::Calibrate()
 {
     Raw_Data accel, gyro;
-
-    vTaskDelay(1000 / portTICK_RATE_MS);
 
     for (uint16_t i = 0; i < 500; i++)
     {

commit c00d713c6dd2987aff598b7f593fa9cdf1fd36f8

(IMU): add a small delay before sensor calibration
Fixes bug when sensor returns non-zero values right after calibration, caused by https://github.com/michprev/flyhero-esp32/commit/41ede7a07683c62b740bcd5c37994b2f75dc628b

IMU/main/MPU6000.cpp
@@ -342,6 +342,8 @@ void MPU6000::Init()
 
     ESP_ERROR_CHECK(spi_bus_add_device(HSPI_HOST, &devcfg, &this->spi));
 
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+
     this->Calibrate();
 
     this->ready = true;

IMU/main/MPU6050.cpp
@@ -397,6 +397,8 @@ void MPU6050::Init()
 
     ESP_ERROR_CHECK(this->set_interrupt(true));
 
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+
     this->Calibrate();
 
     this->ready = true;

IMU/main/MPU9250.cpp
@@ -384,6 +384,8 @@ void MPU9250::Init()
 
     ESP_ERROR_CHECK(spi_bus_add_device(HSPI_HOST, &devcfg, &this->spi));
 
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+
     this->Calibrate();
 
     this->ready = true;