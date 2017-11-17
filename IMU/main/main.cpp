80aca3a078e11c7260237da4f2af905099351f0c
michprev <michprev@gmail.com>
2017-11-17 19:12:01 +0100

    (I2C issue): minimalize test code

--- IMU/main/main.cpp
+++ IMU/main/main.cpp
@@ -8,19 +8,15 @@
 
 using namespace flyhero;
 
-QueueHandle_t data_queue;
 
 void imu_task(void *args);
-void log_task(void *args);
 
 
 extern "C" void app_main(void)
 {
-
-    data_queue = xQueueCreate(10, sizeof(IMU::Euler_Angles));
-
-    xTaskCreatePinnedToCore(log_task, "Log task", 4096, NULL, 2, NULL, 1);
     xTaskCreatePinnedToCore(imu_task, "IMU task", 4096, NULL, 2, NULL, 0);
+
+    while (true);
 }
 
 void imu_task(void *args)
@@ -30,43 +26,13 @@ void imu_task(void *args)
 
     imu->Init();
 
-    //Mahony_Filter mahony(2, 0.1f, imu->Get_Sample_Rate());
-    Complementary_Filter complementary(0.995f, imu->Get_Sample_Rate());
-
     IMU::Sensor_Data accel, gyro;
-    IMU::Euler_Angles euler;
-
-    uint8_t i = 0;
 
     while (true)
     {
         if (imu->Data_Ready())
         {
             imu->Read_Data(accel, gyro);
-
-            //mahony.Compute(accel, gyro, euler);
-            complementary.Compute(accel, gyro, euler);
-
-            i++;
-
-            if (i % 100 == 0)
-            {
-                i = 0;
-
-                if (xQueueSendToBack(data_queue, &euler, 0) != pdTRUE)
-                    break;
-            }
         }
-    }
-}
-
-void log_task(void *args)
-{
-    IMU::Euler_Angles euler;
-
-    while (true)
-    {
-        if (xQueueReceive(data_queue, &euler, 0) == pdTRUE)
-            std::cout << euler.roll << " " << euler.pitch << " " << euler.yaw << std::endl;
     }
 }
