michprev/flyhero-esp32

commit a45245656051c0b9038e9920f7a7ee0e98f82655

Replace gettimeofday by esp_timer_get_time

Ultrasonic/main/HC_SR04.cpp
@@ -63,19 +63,18 @@ void HC_SR04::Init()
 
 void HC_SR04::Trigger()
 {
-    timeval start, end;
+    int64_t start, end;
 
     // we should not get errors here so no need to check
     gpio_set_level(this->trigg_pin, 1);
-    gettimeofday(&start, NULL);
+    start = esp_timer_get_time();
 
     uint32_t delta;
 
     do
     {
-        gettimeofday(&end, NULL);
-        delta = (end.tv_sec - start.tv_sec) * 1000000 + end.tv_usec - start.tv_usec;
-    } while (delta < 10);
+        end = esp_timer_get_time();
+    } while (end - start < 10);
 
     gpio_set_level(this->trigg_pin, 0);
 }

Ultrasonic/main/HC_SR04.h
@@ -10,7 +10,6 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/semphr.h>
 #include <driver/gpio.h>
-#include <sys/time.h>
 #include <esp_err.h>
 
 

IMU/main/Complementary_Filter.cpp
@@ -14,8 +14,7 @@ namespace flyhero
 Complementary_Filter::Complementary_Filter(double coeff)
         : COMPLEMENTARY_COEFFICIENT(coeff)
 {
-    this->last_time.tv_sec = 0;
-    this->last_time.tv_usec = 0;
+    this->last_time = 0;
     this->euler.roll = 0;
     this->euler.pitch = 0;
     this->euler.yaw = 0;
@@ -25,19 +24,16 @@ void Complementary_Filter::Compute(IMU::Sensor_Data accel, IMU::Sensor_Data gyro
 {
     double delta_t;
 
-    if (this->last_time.tv_sec == 0 && this->last_time.tv_usec == 0)
+    if (this->last_time == 0)
     {
         delta_t = 0;
 
-        gettimeofday(&this->last_time, NULL);
+        this->last_time = esp_timer_get_time();
     }
     else
     {
-        timeval tmp;
-
-        gettimeofday(&tmp, NULL);
-
-        delta_t = tmp.tv_sec - this->last_time.tv_sec + (tmp.tv_usec - this->last_time.tv_usec) * 0.000001;
+        int64_t tmp = esp_timer_get_time();
+        delta_t = (tmp - this->last_time) * 0.000001;
 
         this->last_time = tmp;
     }
@@ -65,8 +61,7 @@ void Complementary_Filter::Compute(IMU::Sensor_Data accel, IMU::Sensor_Data gyro
 
 void Complementary_Filter::Reset()
 {
-    this->last_time.tv_sec = 0;
-    this->last_time.tv_usec = 0;
+    this->last_time = 0;
 
     this->euler.roll = 0;
     this->euler.pitch = 0;

IMU/main/Complementary_Filter.h
@@ -7,7 +7,7 @@
 
 #pragma once
 
-#include <sys/time.h>
+#include <esp_timer.h>
 
 #include "Fusion_Filter.h"
 #include "Math.h"
@@ -20,7 +20,7 @@ class Complementary_Filter : public Fusion_Filter
 {
 private:
     IMU::Euler_Angles euler;
-    timeval last_time;
+    int64_t last_time;
     const double COMPLEMENTARY_COEFFICIENT;
 
 public:

IMU/main/Mahony_Filter.cpp
@@ -14,8 +14,7 @@ namespace flyhero
 Mahony_Filter::Mahony_Filter(double kp, double ki)
         : MAHONY_KP(kp), MAHONY_KI(ki)
 {
-    this->last_time.tv_sec = 0;
-    this->last_time.tv_usec = 0;
+    this->last_time = 0;
     this->quaternion.q0 = 1;
     this->quaternion.q1 = 0;
     this->quaternion.q2 = 0;
@@ -30,19 +29,16 @@ void Mahony_Filter::Compute(IMU::Sensor_Data accel, IMU::Sensor_Data gyro, IMU::
 {
     double delta_t;
 
-    if (this->last_time.tv_sec == 0 && this->last_time.tv_usec == 0)
+    if (this->last_time == 0)
     {
         delta_t = 0;
 
-        gettimeofday(&this->last_time, NULL);
+        this->last_time = esp_timer_get_time();
     }
     else
     {
-        timeval tmp;
-
-        gettimeofday(&tmp, NULL);
-
-        delta_t = tmp.tv_sec - this->last_time.tv_sec + (tmp.tv_usec - this->last_time.tv_usec) * 0.000001;
+        int64_t tmp = esp_timer_get_time();
+        delta_t = (tmp - this->last_time) * 0.000001;
 
         this->last_time = tmp;
     }
@@ -139,8 +135,7 @@ void Mahony_Filter::Compute(IMU::Sensor_Data accel, IMU::Sensor_Data gyro, IMU::
 
 void Mahony_Filter::Reset()
 {
-    this->last_time.tv_sec = 0;
-    this->last_time.tv_usec = 0;
+    this->last_time = 0;
 
     this->quaternion.q0 = 1;
     this->quaternion.q1 = 0;

IMU/main/Mahony_Filter.h
@@ -8,7 +8,7 @@
 #pragma once
 
 #include <cmath>
-#include <sys/time.h>
+#include <esp_timer.h>
 
 #include "Fusion_Filter.h"
 #include "Math.h"
@@ -22,7 +22,7 @@ class Mahony_Filter : public Fusion_Filter
 private:
     IMU::Quaternion quaternion;
     const double MAHONY_KP, MAHONY_KI;
-    timeval last_time;
+    int64_t last_time;
     IMU::Sensor_Data error_integral;
 
 public:

The_Eye/main/PID.cpp
@@ -15,8 +15,7 @@ namespace flyhero
 PID::PID(double update_rate, double i_max, double Kp, double Ki, double Kd)
         : d_term_lpf(Biquad_Filter::FILTER_LOW_PASS, update_rate, 20)
 {
-    this->last_t.tv_sec = 0;
-    this->last_t.tv_usec = 0;
+    this->last_t = 0;
     this->integrator = 0;
     this->Kp = Kp;
     this->Ki = Ki;
@@ -28,20 +27,19 @@ PID::PID(double update_rate, double i_max, double Kp, double Ki, double Kd)
 
 double PID::Get_PID(double error)
 {
-    timeval now;
-    gettimeofday(&now, NULL);
+    int64_t now = esp_timer_get_time();
 
-    double delta_t = now.tv_sec - this->last_t.tv_sec
-               + (now.tv_usec - this->last_t.tv_usec) * 0.000001;
-    double output = 0;
+    double delta_t = (now - this->last_t) * 0.000001;
 
-    if (this->last_t.tv_sec == 0 || delta_t > 1)
+    if (this->last_t == 0 || delta_t > 1)
     {
         this->integrator = 0;
         delta_t = 0;
     }
 
     this->last_t = now;
+
+    double output = 0;
 
     // proportional component
     output += error * this->Kp;

The_Eye/main/PID.h
@@ -7,7 +7,7 @@
 
 #pragma once
 
-#include <sys/time.h>
+#include <esp_timer.h>
 #include <cmath>
 
 #include "Biquad_Filter.h"
@@ -20,7 +20,7 @@ class PID
 {
 private:
     Biquad_Filter d_term_lpf;
-    timeval last_t;
+    int64_t last_t;
     double Kp, Ki, Kd;
     double integrator;
     double i_max;

commit 4b14d3bd309ca472c782e541c935f26b3ad950cd

(HC-SR04): fix project won't compile
Caused by https://github.com/michprev/flyhero-esp32/commit/a45245656051c0b9038e9920f7a7ee0e98f82655#diff-bd3bea910af8581fa177e9ae041aeff9

Ultrasonic/main/HC_SR04.cpp
@@ -69,8 +69,6 @@ void HC_SR04::Trigger()
     gpio_set_level(this->trigg_pin, 1);
     start = esp_timer_get_time();
 
-    uint32_t delta;
-
     do
     {
         end = esp_timer_get_time();
@@ -96,15 +94,14 @@ void HC_SR04::Echo_Callback()
     this->level_high = !this->level_high;
 
     if (this->level_high)
-        gettimeofday(&this->start, NULL);
+        this->start = esp_timer_get_time();
     else
     {
-        timeval end;
-        gettimeofday(&end, NULL);
+        int64_t end = esp_timer_get_time();
 
         while (xSemaphoreTake(this->distance_semaphore, 0) != pdTRUE);
 
-        this->distance = (end.tv_sec - this->start.tv_sec + (end.tv_usec - this->start.tv_usec) * 0.000001) * 334 * 0.5;
+        this->distance = (end - this->start) * 0.000001 * 334 * 0.5;
 
         xSemaphoreGive(this->distance_semaphore);
     }

Ultrasonic/main/HC_SR04.h
@@ -11,6 +11,7 @@
 #include <freertos/semphr.h>
 #include <driver/gpio.h>
 #include <esp_err.h>
+#include <esp_timer.h>
 
 
 namespace flyhero
@@ -23,7 +24,7 @@ private:
     const gpio_num_t echo_pin;
     const gpio_isr_t echo_handler;
 
-    timeval start;
+    int64_t start;
     bool level_high;
     double distance;
     SemaphoreHandle_t distance_semaphore;